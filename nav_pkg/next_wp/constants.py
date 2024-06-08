c9dc319971bb60320d2e5ec46e10ef4e3b8a9a0f
xAspox <a.dellaporta28@studenti.unisa.it>
2024-06-08 13:44:06 +0200

    Aggiunta delle coordinate dei punti nel file constants

--- nav_pkg/next_wp/constants.py
+++ nav_pkg/next_wp/constants.py
@@ -12,3 +12,15 @@ class Signs(enum):
     LEFT = 'LEFT'
     STRAIGHTON = 'STRAIGHTON'
     GOBACK = 'GOBACK'
+
+class Coordinates(enum): 
+    A = [-22.9, 0.044]
+    B = [-23.74, -8.72]
+    C = [-3.11, 0.11]
+    D = [-3.3, -9.51]
+    E = [16.64, 0.03]
+    F = [16.8, -9.66]
+    G = [36.72, -0.88]
+    H = [36.70, -10]
+    I = [58, -2]
+    L = [58, -11]
